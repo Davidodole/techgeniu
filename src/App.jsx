c19aaa33f117e54c27418506318a6a5e6058cb8e
Davidodole <dola7468@gmail.com>
2024-02-03 13:15:26 +0100

    changing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
+<<<<<<< HEAD
 import About from "./pages/About"
 import Contact from "./pages/Contact"
 import Home from "./pages/home";
+=======
+import About from "./pages/About.jsx"
+import Contact from "./pages/Contact.jsx"
+import Home from "./pages/Home.jsx";
+>>>>>>> 0601dae (changes)
 
 
 export default function App (){
