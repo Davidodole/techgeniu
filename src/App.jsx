fe25c3e1f5f67f5e52fd2dd1582d9bb4061dcbb5
Davidodole <124046210+Davidodole@users.noreply.github.com>
2024-02-03 04:39:23 -0800

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,17 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
 import About from "./pages/About"
 import Contact from "./pages/Contact"
-import Home from "./pages/home";
-=======
-import About from "./pages/About.jsx"
-import Contact from "./pages/Contact.jsx"
-import Home from "./pages/Home.jsx";
->>>>>>> 0601dae (changes)
-
+import Home from "./pages/Home";
 
 export default function App (){
   return(
     <HashRouter>
-    <Routes>
-    <Route index element={<Home />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
+      <Routes>
+        <Route index element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </HashRouter>
   )
 }
